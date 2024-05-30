75288098e38d335f01c3e7ca21f372da525cf2b4
Jessica-Asamoah <jessicaasamoah903@gmail.com>
2024-05-30 17:24:45 +0000

    user dropdown menu

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -7,7 +7,9 @@ const SignIn = () => {
 		<div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
 			<div className="flex flex-col space-y-2 text-center">
 				<Icons.logo className="mx-auto h-6 w-6" />
-				<h1 className="text-2xl font-semibold tracking-tight">Welcome Back!</h1>
+				<h1 className="text-2xl font-semibold tracking-tight">
+					Welcome Back Buzzie!
+				</h1>
 
 				{/* Sign In Form */}
 				<UserAuthForm />
--- frontend/src/components/UserAvatar.tsx
+++ frontend/src/components/UserAvatar.tsx
@@ -1,12 +1,34 @@
+import { Avatar, AvatarFallback } from './ui/Avatar';
+import { AvatarProps } from '@radix-ui/react-avatar';
 import { User } from 'next-auth';
 import { FC } from 'react';
+import Image from 'next/image';
+import { Icons } from './Icons';
 
-interface UserAvatarProps {
+interface UserAvatarProps extends AvatarProps {
 	user: Pick<User, 'name' | 'image'>;
 }
 
-const UserAvatar: FC<UserAvatarProps> = ({ user }) => {
-	return <div>UserAvatar</div>;
+const UserAvatar: FC<UserAvatarProps> = ({ user, ...props }) => {
+	return (
+		<Avatar {...props}>
+			{user.image ? (
+				<div className="relative aspect-square h-full w-full">
+					<Image
+						fill
+						src={user.image}
+						alt="profile picture"
+						referrerPolicy="no-referrer"
+					/>
+				</div>
+			) : (
+				<AvatarFallback>
+					<span className="sr-only">{user?.name}</span>
+					<Icons.user className="h-4 w-4" />
+				</AvatarFallback>
+			)}
+		</Avatar>
+	);
 };
 
 export default UserAvatar;
